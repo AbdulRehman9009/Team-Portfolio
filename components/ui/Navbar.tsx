55d7dc0a324afe53b9a75b04cfe4398418b339c0
AbdulRehman9009 <opabdul636@gmail.com>
2026-02-08 18:29:50 +0500

    Initial commit from Create Next App

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    const navItems = [
+        { name: 'Team Members', link: '/' },
+        { name: 'Projects', link: '/projects' },
+        { name: 'About', link: '/about' },
+        { name: 'Contact', link: '/contact' },
+        
+    ]
+  return (
+    <nav>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
